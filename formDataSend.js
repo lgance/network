7a196b3cf6b5b40f06c00fdaa6a9398c99bb25fd
jaeuk.lee <dlwodnrsms@naver.com>
2020-02-20 22:02:38 +0900

    typescript multer prototype

---
+++ formDataSend.js
@@ -0,0 +1,29 @@
+
+const FormData = require('form-data');
+const fs = require('fs');
+const axios = require('axios');
+
+/** define config variable  */
+const uploadFilePath = 'jest.config.js';
+const uploadUrl = 'http://localhost/upload';
+// const uploadUrl = `http://106.10.51.104:9428/upload/`;
+/** Upload Stream Settings */
+const stream = fs.createReadStream(uploadFilePath);
+
+/** Form Settings  */
+const form = new FormData();
+form.append('dump',stream);
+const formHeaders = form.getHeaders();
+
+/** Multi-part Form-Data upload */
+axios.post(uploadUrl,form,{
+  headers:{
+    ...formHeaders
+  }  
+})
+.then(res=>console.log(res))
+.catch(err=>console.error(err));
+
+
+
+
--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,9 +1,26 @@
 import * as express from 'express';
 import * as multer from 'multer';
+import * as moment from 'moment';
+
+import * as path from 'path';
 import Controller from '../interface/controller.interface'
 
 
-const upload = multer({dest:'../uploads/'});
+// const upload = multer({dest:'../../uploads/'});
+const storage = multer.diskStorage({
+  destination:function(req:Express.Request,file:Express.Multer.File,cb:Function){
+    cb(null, 'uploads')
+  },
+  filename:function(req:Express.Request,file:Express.Multer.File,cb:Function){
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const extName:string = path.extname(file.originalname);
+    const createDate:string = moment().format('YYYY_MM_DD-HH_mm_ss');
+    const fullPath:string = file.fieldname +'-'+ createDate +'_'+ uniqueSuffix.substring(0,4) + extName;
+    cb(null, fullPath);
+  }
+})
+
+const upload = multer({storage:storage});
 
 class UploadController implements Controller {
   public path='/upload';
@@ -14,18 +31,30 @@ class UploadController implements Controller {
   }
 
   private initializeRoutes(){
-    this.router.post(this.path,this.uploadControllerIndexPost);
+    this.router.post(this.path,upload.array('dump'),this.uploadControllerIndexPost);
     this.router.get(this.path,this.uploadControllerIndex);
   }
 
-  private uploadControllerIndex = async (req:express.Request,res:express.Response)=>{
+  private uploadControllerIndex = async (req:express.Request,res:express.Response,next:express.NextFunction)=>{
+      console.log('upload Controller Index [GET]')
       res.send('UploadController Index');
   }
 
-  private uploadControllerIndexPost = async(req:express.Request,res:express.Response)=>{
-      res.send('UploadController Post Index');
+  private uploadControllerIndexPost = async(req:express.Request,res:express.Response,next:express.NextFunction)=>{
+      console.log(`[FILES] `);
+      
+      let _files:string='';
+
+      function isFile(data:{[fieldname: string]: Express.Multer.File[]} | Express.Multer.File[]) : data is Express.Multer.File[] {
+         return Array.isArray(data);
+      }
+           
+      if(isFile(req.files)){
+          req.files.forEach((item:Express.Multer.File,index:number)=>{
+              _files = _files.concat(`${item.originalname} `);
+          });
+      }
+      res.send(`Upload Complete is  [${_files}]`);
   }
 }
 export default UploadController;
-
-
